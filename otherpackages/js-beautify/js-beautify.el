d3e696d4ffa5bb44ad6dcc9e2d6d8a33c8c727c7
Iqbal Ansari <iqbal0212@yahoo.com>
2013-10-06 02:25:11 +0530

    Added js-beautify

---
+++ otherpackages/js-beautify/js-beautify.el
@@ -0,0 +1,27 @@
+;;; js-beautify.el -- beautify some js code
+;;; Taken from [http://sethmason.com/2011/04/28/jsbeautify-in-emacs.html]
+(defgroup js-beautify nil
+  "Use jsbeautify to beautify some js"
+  :group 'editing)
+
+(defcustom js-beautify-args "--jslint-happy --brace-style=end-expand"
+  "Arguments to pass to jsbeautify script"
+  :type '(string)
+  :group 'js-beautify)
+
+(defun js-beautify ()
+  "Beautify a region of javascript using the code from jsbeautify.org"
+  (interactive)
+  (let ((orig-point (point)))
+    (unless (mark)
+      (mark-defun))
+    (shell-command-on-region (point)
+                             (mark)
+                             (concat "js-beautify "
+                                     " --stdin "
+                                     js-beautify-args)
+                             nil t)
+    (goto-char orig-point)))
+
+(provide 'js-beautify)
+;;; js-beautify.el ends here
