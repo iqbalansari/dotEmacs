a261079135855f7b331db2e1503b3cf3a7dd1163
Iqbal Ansari <iqbal0212@yahoo.com>
2013-11-24 19:43:18 +0530

    Cleaned up snippets folder

--- snippets/c-mode/.yas-compiled-snippets.el
+++
@@ -1,9 +0,0 @@
-;;; Compiled snippets and support files for `c-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'c-mode
-					 '(("fopen" "FILE *${fp} = fopen(${\"file\"}, \"${r}\");" "FILE *fp = fopen(..., ...);" nil nil nil nil nil nil)
-					   ("printf" "printf (\"${1:%s}\\\\n\"${1:$(if (string-match \"%\" yas-text) \",\" \"\\);\")\n}$2${1:$(if (string-match \"%\" yas-text) \"\\);\" \"\")}" "printf" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Mon Sep 23 04:51:17 2013
--- snippets/text-mode/.yas-compiled-snippets.el
+++
@@ -1,9 +0,0 @@
-;;; Compiled snippets and support files for `text-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'text-mode
-					 '(("email" "`(replace-regexp-in-string \"@\" \"@NOSPAM.\" user-mail-address)`" "(user's email)" nil nil nil nil nil nil)
-					   ("time" "`(current-time-string)`" "(current time)" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Mon Sep 23 04:51:18 2013
