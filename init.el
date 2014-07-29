31f59897bbc171fbbbc0c71c3ca7a28deb75ded6
Iqbal Ansari <iqbalansari02@yahoo.com>
2014-07-29 16:16:41 +0530

    Minor changes

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 ;; Save custom variables in separate file
 (setq custom-file "~/.emacs.d/custom.el")
+
 (when (file-exists-p custom-file)
   (load custom-file))
 
@@ -13,13 +14,22 @@
 ;; Disable vc for Git repositories
 (delete 'Git vc-handled-backends)
 
-;; Memory is cheap, stop the world only after 20 megabytes are used
-(setq gc-cons-threshold (* 20 1024 1024))
+;; Memory is cheap, stop the world only after 100 megabytes are used
+(setq gc-cons-threshold (* 100 1024 1024))
 
 ;; Use org babel to load rest of the configuration
 (require 'org)
 (require 'cl-lib)
-(org-babel-load-file "~/.emacs.d/iqbal-init.org")
+
+;; Create .compiled dir in modules
+(unless (file-exists-p "~/.emacs.d/modules/.compiled/")
+  (make-directory "~/.emacs.d/modules/.compiled/"))
+
+;; Avoid re-extraction if file has not changed
+(if (file-newer-than-file-p "~/.emacs.d/iqbal-init.org" 
+			    "~/.emacs.d/iqbal-init.el")
+    (org-babel-load-file "~/.emacs.d/iqbal-init.org")
+  (load-file "~/.emacs.d/iqbal-init.el"))
 
 ;; Load private settings if exist
 (when (file-exists-p "~/.emacs.d/private.org")
