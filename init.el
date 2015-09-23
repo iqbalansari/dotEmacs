46a2fcd4567bff515ae2f84bc30e6b59b06c5d98
Iqbal Ansari <iqbal.ansari@ignitesol.com>
2015-09-23 12:36:44 +0530

    Let bound gc-cons-threshhold only for startup files require subr-x when required

--- init.el
+++ init.el
@@ -44,18 +44,19 @@ your emacs and try again!"))
 
 ;; No special handling of files at startup
 ;; Credits: https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
-(let (file-name-handler-alist)
+(let ((gc-cons-threshold (* 100 1024 1024))
+      (file-name-handler-alist '(("\\.gz" . jka-compr-handler))))
 
   ;; Avoid re-extraction if file has not changed
-  (if (file-newer-than-file-p (locate-user-emacs-file "emacs-init.org") 
+  (if (file-newer-than-file-p (locate-user-emacs-file "emacs-init.org")
                               (locate-user-emacs-file "emacs-init.el"))
       (org-babel-load-file (locate-user-emacs-file "emacs-init.org"))
     (load-file (locate-user-emacs-file "emacs-init.el")))
-  
+
   ;; Load private settings if exist
   (when (file-exists-p (locate-user-emacs-file "private.org"))
     ;; But avoid re-extraction if the file has not changed
-    (if (file-newer-than-file-p (locate-user-emacs-file "private.org") 
+    (if (file-newer-than-file-p (locate-user-emacs-file "private.org")
                                 (locate-user-emacs-file "private.el"))
         (org-babel-load-file (locate-user-emacs-file "private.org"))
       (load-file (locate-user-emacs-file "private.el")))))
